4b31ac64e1e7e73dc6099a67a016a385b76b0706
Klaus Reimer <k@ailis.de>
2022-02-20 16:38:11 +0100

    Only use globalThis as every platform supports this now

--- src/main/util/global.ts
+++ src/main/util/global.ts
@@ -9,12 +9,7 @@ import { IllegalArgumentException } from "./exception";
  * The global scope of the current environment. This is `globalThis` if present, the browsers Window object when
  * present or the Node.js global object. When nothing of all this exists then an empty object is returned as fallback.
  */
-const globalScope = (
-    typeof globalThis !== "undefined" ? globalThis :
-    typeof window !== "undefined" ? window :
-    typeof global !== "undefined" ? global :
-    {}
-) as typeof globalThis & Window & Record<string, unknown>;
+const globalScope = globalThis as typeof globalThis & Window & Record<string, unknown>;
 
 export { globalScope as global };
 
